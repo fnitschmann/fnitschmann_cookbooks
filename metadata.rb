ab4ed8ca0409aff5f834d60185d9d7301cad3943
Florian Nitschmann <f.nitschmann@googlemail.com>
2013-04-01 18:50:12 +0200

    Added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name 							"fnitschmann-env"
+maintainer 				"Florian Nitschmann"
+maintainer_email 	"f.nitschmann@googlemail.com"
+license 					"MIT"
+description				"Install envoironment for fnitschmann"
+version						"1.0"
